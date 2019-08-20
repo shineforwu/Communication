806b9dc24dc43914ba5454af30f062ad8feb5c24
Shine <331046212@qq.com>
2019-08-20 13:21:09 +0800

    Add WebAPi

---
+++ WebApi/WepApiHostSelf/Controllers/TestController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace ConsoleHostWebApi.Controllers
+{
+    public class TestController : ApiController
+    {
+
+        // GET api/<controller>/5
+        [HttpGet]
+        public Object Get()
+        {
+
+
+            return "{\r\n  \"Name\": \"ABC\"\r\n}";
+        }
+
+        // POST api/<controller>
+        [HttpPost]
+        public Object Post(Object value)
+        {
+            return value.ToString();
+        }
+
+        // PUT api/<controller>/5
+        [HttpPut]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/<controller>/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ WebApi/WepApiHostSelf/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.SelfHost;
+
+namespace WepApiHostSelf
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                var config = new HttpSelfHostConfiguration("http://0.0.0.0:8889");
+                config.Routes.MapHttpRoute(
+                    "API Default", "api/{controller}/{Action}",
+                    new { id = RouteParameter.Optional });
+                HttpSelfHostServer server = new HttpSelfHostServer(config);
+                config.Formatters.Add(new System.Net.Http.Formatting.JsonMediaTypeFormatter());//Add MediaType
+                server.OpenAsync().Wait();
+                Console.WriteLine("Start Web Api");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.ReadKey();
+        }
+    }
+}
