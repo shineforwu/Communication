ccbcd8bf497770d0a94be9df7145906fc5aaaa1c
jiawei.wu <jiaweiwu@smacon.cn>
2022-11-17 13:47:16 +0800

    add prot

---
+++ SerialPortCommon/ISerialPortHelper.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SerialPortCommon
+{
+    public abstract partial class ISerialPortHelper:IDisposable
+    {
+        
+        public SerialPort MySerialPort { get; set; }=new SerialPort();
+        public bool IsHeartbeatOK { get; set; } = true;
+
+        public virtual void Dispose()
+        {
+            if(MySerialPort != null)
+            {
+                MySerialPort.Close();
+                MySerialPort.Dispose();
+            }
+        }
+
+        public virtual bool SendCmd(byte[] data)
+        {
+            throw new NotImplementedException();
+        }
+
+        public virtual void DataReceivedHandler(object sender,SerialDataReceivedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
